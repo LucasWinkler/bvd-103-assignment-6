2c5372a0a9bd285c37b947f5706899ef4af9229b
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-09 15:21:10 -0400

    Setup scripts and workspaces in package.json files

--- generate-openapi.sh
+++
@@ -1,6 +0,0 @@
-(cd warehouse && npx tsoa spec-and-routes)
-(cd orders && npx tsoa spec-and-routes)
-
-npx swagger-merge -i warehouse/build/swagger.json -i orders/build/swagger.json -o docs/swagger.json
-
-npx @openapitools/openapi-generator-cli generate -i ./docs/swagger.json -o ./client -g typescript-fetch
