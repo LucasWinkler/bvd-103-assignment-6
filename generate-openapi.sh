f06c2a121cdd5acc311c008a3359bb1f50e96f4b
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-09 18:43:02 +0000

    Move subfolders to packages folder for better organization

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -1,2 +1,6 @@
-npx tsoa spec-and-routes
-npx @openapitools/openapi-generator-cli generate -i ./build/swagger.json -o ./client -g typescript-fetch
+(cd warehouse && npx tsoa spec-and-routes)
+(cd orders && npx tsoa spec-and-routes)
+
+npx swagger-merge -i warehouse/build/swagger.json -i orders/build/swagger.json -o docs/swagger.json
+
+npx @openapitools/openapi-generator-cli generate -i ./docs/swagger.json -o ./client -g typescript-fetch
