39f460a993181e9974f4b5901ab6c3ea0debc0f3
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-10 23:36:28 +0000

    Cleanup docs package.json

--- packages/docs/server.ts
+++ packages/docs/server.ts
@@ -1,16 +1,16 @@
-import Koa from 'koa';
-import cors from '@koa/cors';
-import swagger from './build/swagger.json';
-import { koaSwagger } from 'koa2-swagger-ui';
-import { type Server, type IncomingMessage, type ServerResponse } from 'http';
+import Koa from 'koa'
+import cors from '@koa/cors'
+import swagger from './build/swagger.json'
+import { koaSwagger } from 'koa2-swagger-ui'
+import { type Server, type IncomingMessage, type ServerResponse } from 'http'
 
 export default async function (
   port?: number
 ): Promise<{ server: Server<typeof IncomingMessage, typeof ServerResponse> }> {
-  const app = new Koa();
+  const app = new Koa()
 
   // And we add cors to ensure we can access our API from the mcmasterful-books website
-  app.use(cors());
+  app.use(cors())
 
   app.use(
     koaSwagger({
@@ -18,13 +18,13 @@ export default async function (
       specPrefix: '/docs/spec',
       exposeSpec: true,
       swaggerOptions: {
-        spec: swagger,
-      },
+        spec: swagger
+      }
     })
-  );
+  )
   return {
     server: app.listen(port, () => {
-      console.log('listening');
-    }),
-  };
+      console.log('listening')
+    })
+  }
 }
