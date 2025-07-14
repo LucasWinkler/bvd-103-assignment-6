c4eed44448ada1987d7f8375d974f3821beee41e
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-14 00:39:59 +0000

    Move old books routes to a routes folder

--- packages/books/src/routes/index.ts
+++ packages/books/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { type ZodRouter } from 'koa-zod-router'
-import deleteBook from './services/delete'
-import getBookRoute from './services/lookup'
-import { type BookDatabaseAccessor } from './data/database_access'
+import deleteBook from './delete'
+import getBookRoute from './lookup'
+import { type BookDatabaseAccessor } from '../data/database_access'
 
 // TODO: Probably convert this to tsoa routes
 export function setupBookRoutes (router: ZodRouter, books: BookDatabaseAccessor): void {
